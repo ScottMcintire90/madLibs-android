d1ad3bc9a0248c0ac0e3887dc5dd73a5990ba3e4
jf <j.k.fokes@gmail.com>
2016-06-27 13:32:48 -0700

    Basic Tests Passing

---
+++ app/src/androidTest/java/com/epicodus/madlibs/MainActivityInstrumentationTest.java
@@ -0,0 +1,24 @@
+package com.epicodus.madlibs;
+
+import android.support.test.rule.ActivityTestRule;
+
+import org.junit.Rule;
+import org.junit.Test;
+
+import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.action.ViewActions.typeText;
+import static android.support.test.espresso.assertion.ViewAssertions.matches;
+import static android.support.test.espresso.matcher.ViewMatchers.withId;
+import static android.support.test.espresso.matcher.ViewMatchers.withText;
+
+public class MainActivityInstrumentationTest {
+    @Rule
+    public ActivityTestRule<MainActivity> activityTestRule =
+            new ActivityTestRule<>(MainActivity.class);
+
+    @Test
+    public void validateUserName() {
+        onView(withId(R.id.name)).perform(typeText("Charlie Murphy"))
+                .check(matches(withText("Charlie Murphy")));
+    }
+}
