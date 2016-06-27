ec2bfa8d7cf43ffbfc2f768e6f5a311f18f2df80
jf <j.k.fokes@gmail.com>
2016-06-27 14:18:11 -0700

    Add Home Button

--- app/src/androidTest/java/com/epicodus/madlibs/MainActivityInstrumentationTest.java
+++ app/src/androidTest/java/com/epicodus/madlibs/MainActivityInstrumentationTest.java
@@ -6,6 +6,7 @@ import org.junit.Rule;
 import org.junit.Test;
 
 import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.action.ViewActions.click;
 import static android.support.test.espresso.action.ViewActions.typeText;
 import static android.support.test.espresso.assertion.ViewAssertions.matches;
 import static android.support.test.espresso.matcher.ViewMatchers.withId;
@@ -16,9 +17,29 @@ public class MainActivityInstrumentationTest {
     public ActivityTestRule<MainActivity> activityTestRule =
             new ActivityTestRule<>(MainActivity.class);
 
+//    @Test
+//    public void validateUserName() {
+//        onView(withId(R.id.name)).perform(typeText("Charlie Murphy"))
+//                .check(matches(withText("Charlie Murphy")));
+//    }
+
     @Test
-    public void validateUserName() {
-        onView(withId(R.id.name)).perform(typeText("Charlie Murphy"))
-                .check(matches(withText("Charlie Murphy")));
+    public void inputIsSentToStoryActivity() {
+        String name = "Charlie Murphy";
+        String noun = "Chair";
+        String verb = "walking";
+        String number = "12";
+        String animal = "Cat";
+        onView(withId(R.id.name)).perform(typeText(name));
+        onView(withId(R.id.noun)).perform(typeText(noun));
+        onView(withId(R.id.animal)).perform(typeText(animal));
+        onView(withId(R.id.number)).perform(typeText(number));
+        onView(withId(R.id.verb)).perform(typeText(verb));
+        onView(withId(R.id.submitButton)).perform(click());
+        onView(withId(R.id.story)).check(matches(withText(name + " was walking along and saw a " + animal +" " + verb +" " + number + " times on a " + noun + ".")));
+
+
+
     }
+
 }
--- app/src/main/java/com/epicodus/madlibs/StoryActivity.java
+++ app/src/main/java/com/epicodus/madlibs/StoryActivity.java
@@ -4,12 +4,15 @@ package com.epicodus.madlibs;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 import org.w3c.dom.Text;
 
 public class StoryActivity extends AppCompatActivity {
     private TextView mStory;
+    private Button mHomeButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,12 +20,20 @@ public class StoryActivity extends AppCompatActivity {
         setContentView(R.layout.activity_story);
         mStory = (TextView) findViewById(R.id.story);
         Intent intent = getIntent();
-        String noun = intent.getStringExtra("noun").toLowerCase();
+        String noun = intent.getStringExtra("noun");
         String name = intent.getStringExtra("name");
-        String verb = intent.getStringExtra("verb").toLowerCase();
-        String animal = intent.getStringExtra("animal").toLowerCase();
-        String number = intent.getStringExtra("number").toLowerCase();
+        String verb = intent.getStringExtra("verb");
+        String animal = intent.getStringExtra("animal");
+        String number = intent.getStringExtra("number");
+        mStory.setText(name + " was walking along and saw a " + animal +" " + verb +" " + number + " times on a " + noun + ".");
 
-        mStory.setText(name + " was walking along and " + verb + " a " + animal + " " + number + " times on a " + noun + ".");
+        mHomeButton = (Button) findViewById(R.id.homeButton);
+        mHomeButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(StoryActivity.this, MainActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
